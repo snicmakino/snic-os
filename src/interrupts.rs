b3d51fd45db96b6d9e236390e4ce4d76c850e6e8
snicmakino <snicmakino@gmail.com>
2022-01-19 04:26:16 +0900

    Introduction to Paging
    殆どがページングについての概要説明。
    ページングとは、メモリの管理手法で、x86-64等のアーキテクチャで採用されている。
    メモリを細かいページという単位に分割して、仮想メモリと物理メモリの対応をページテーブルで行う。x86-64ではページテーブルは4つあり、その仕組みを理解した。
    
    実装の方はpage_fault_handlerを実装。
    書き込みの出来ないメモリアドレスへの書き込み時などに発生するエラー。
    
    次回はページングをカーネルに組み込んでいく作業となる。

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,14 +3,15 @@ use pic8259::ChainedPics;
 use spin;
 use spin::Mutex;
 use x86_64::instructions::port::Port;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
-use crate::{gdt, print, println};
+use crate::{gdt, hlt_loop, print, println};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
@@ -46,6 +47,19 @@ pub fn init_idt() {
 extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Access Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn double_fault_handler(
